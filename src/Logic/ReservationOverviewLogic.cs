b04c786828b1e8473b6ba337ba400116a184e75d
lesley-1303 <1075070@hr.nl>
2024-04-11 14:50:15 +0200

    ik heb de admin de optie gegeven om reservaties van deze week in te laten zien

--- src/Logic/ReservationOverviewLogic.cs
+++ src/Logic/ReservationOverviewLogic.cs
@@ -8,16 +8,22 @@ public static class ReservationOverviewLogic
             MenuHelper.SelectOptions("Choose an option", new Dictionary<string, Action>(){
                 {"1. View all reservations",()=> {
                     ReservationLogic.ViewReservation();
+                    Console.Write($"\n\nPress any key to continue...");
+                    Console.ReadKey();
                 }},
-                {"2. Choose date",()=> {
+                {"2. View all reservations for this week",()=> {
+                    ReservationMenu.ShowSpecificReservation(DateTime.Now.AddDays(7));
+                    Console.Write($"\n\nPress any key to continue...");
+                    Console.ReadKey();
+                }},
+                {"3. Choose date",()=> {
                     DateOnly date = MenuHelper.SelectDate("Select at what date you want to start your reservation:", null, DateOnly.FromDateTime(DateTime.Now), null);
                     DateTime date_2 = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0);
                     ReservationMenu.ShowSpecificReservation(date_2);
                     Console.Write($"\n\nPress any key to continue...");
-                    Console.Write(DateTime.Now);
                     Console.ReadKey();
                 }},
-                {"3. Exit to main menu", ()=>{
+                {"4. Exit to main menu", ()=>{
                     running = false;
                 }},
 
