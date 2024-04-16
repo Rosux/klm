10262583bd09ad550ac15aac63cb2aa250ed24c9
Vendux1 <declanversloot@gmail.com>
2024-04-16 15:37:55 +0200

    Unit test fixed for consumption

--- test/ConsumptionTest.cs
+++ test/ConsumptionTest.cs
@@ -1,41 +1,58 @@
 [TestFixture]
 public class ConsumptionTest
-{   
-    private static ConsumptionAccess? c = new ConsumptionAccess("./TEST1.db");
-    // [SetUp] 
-    // public void Setup()
-    // {   
-    //     c.CloseAllConnections();
-    //     File.Delete("./TEST1.db");
-    // }
-
-    // [TearDown] 
-    // public void Cleanup()
-    // {
-    //     c.CloseAllConnections();    
-    //     File.Delete("./TEST1.db");  
-    // }
+{
+    ConsumptionAccess? c = null;
+    UserAccess? u = null;
+    [OneTimeSetUp]
+    public void Setup(){
+        File.Delete("./TEST.db");
+        c = new ConsumptionAccess("./TEST.db");
+        u = new UserAccess("./TEST.db");
+    }
 
     [Test]
     public void InsertionTest()
     {
         var x = new Consumption(
-            Id :1,
+            Id: 1,
             Name: "Testname",
             Price: 2.50,
             StartTime: new TimeOnly(11,20),
-            EndTime: new TimeOnly(12,20) 
+            EndTime: new TimeOnly(12,20)
         );
-        bool insertresult = c.CreateConsumption(x);
-        c.CloseAllConnections();
-        // Assert.IsTrue(insertresult);
-        // List<Consumption> inserteddata = c.ReadConsumption();
-        // Assert.IsTrue(inserteddata.Any(testdata => testdata.Id == x.Id));
-    }
-        
-    [Test]
-    public void RemoveTest()
-    {
+        bool insertResult = c.CreateConsumption(x);
+        Assert.IsTrue(insertResult, "Failed to insert Consumption.");
 
+        List<Consumption> readResult = c.ReadConsumption();
+        
+        foreach (var consumption in readResult)
+        {
+            if (consumption.Id == x.Id && consumption.Name == x.Name && consumption.Price == x.Price && consumption.StartTime == x.StartTime && consumption.EndTime == x.EndTime)
+            {
+                Assert.Pass("Inserted Consumption found in the list.");
+            }
+        }
+
+        Assert.Fail("Inserted Consumption not found in the list.");
+    }
+    [Test]
+    public void RemoveTest(){
+        var x = new Consumption(
+            Id: 1,
+            Name: "Testname",
+            Price: 2.50,
+            StartTime: new TimeOnly(11,20),
+            EndTime: new TimeOnly(12,20)
+        );
+
+        bool removeResult = c.DeleteConsumption(x);
+        if (removeResult)
+        {
+            Assert.Pass("Consumption successfully removed.");
+        }
+        else
+        {
+            Assert.Fail("Failed to remove Consumption.");
+        }
     }
 }
