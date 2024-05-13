421f23961a030bc123b3c536cb46c7277772dbdb
maktas11 <aktasmuhammed734@gmail.com>
2024-05-13 16:11:26 +0200

    test for new table

--- src/Program.cs
+++ src/Program.cs
@@ -6,9 +6,10 @@ class Program
     public static void Main(){
 
         // USE FOR NOT TESTING
-        Menu.Start();
+        // Menu.Start();
 
         // DO NOT USE UNLESS FOR TESTING
         // Menu.TestStart();
+        UserTable.EditUsers();
     }
 }
