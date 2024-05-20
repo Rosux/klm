125a6b1f89b2d8629bc1be96058295287b641ba3
maktas11 <aktasmuhammed734@gmail.com>
2024-05-20 13:51:30 +0200

    program.cs changed

--- src/Program.cs
+++ src/Program.cs
@@ -4,10 +4,6 @@ class Program
 {
     public static User? CurrentUser = null;
     public static void Main(){
-        // USE FOR NOT TESTING
         Menu.Start();
-
-        // DO NOT USE UNLESS FOR TESTING
-        // Menu.TestStart();
     }
 }
