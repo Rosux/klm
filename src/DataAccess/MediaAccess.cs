cde4acecabab0ec3b3c5b96d8d5fe59cb9aa82a8
Rosux <jcsbt2015@gmail.com>
2024-06-03 16:37:48 +0200

    struct made. getMedia/setMedia made

--- src/DataAccess/MediaAccess.cs
+++ src/DataAccess/MediaAccess.cs
@@ -1,52 +1,59 @@
-using System;
 using Newtonsoft.Json;
 
 public static class MediaAccess
 {
     private static string _mediaPath = Environment.GetEnvironmentVariable("MEDIA_PATH") ?? "";
 
+    /// <summary>
+    /// Saves the given media.
+    /// </summary>
+    /// <param name="media">The Film/Serie object to save.</param>
+    /// <typeparam name="T">Can either be Film or Serie.</typeparam>
+    /// <returns>A boolean indicating if the media got saved.</returns>
     public static bool SaveMedia<T>(T media){
+        // if environment is not set throw an exception
         if(Environment.GetEnvironmentVariable("MEDIA_PATH") == null){
             throw new Exception("Environment MEDIA_PATH not set.");
         }
+        // if the type is not Film or Serie dont save it
         if(typeof(T) != typeof(Film) && typeof(T) != typeof(Serie)){
             return false;
         }
 
-
+        // if the file does not exist: create the file, write the default structure to the file and close the writer
         if(!File.Exists(_mediaPath)){
             File.Create(_mediaPath);
-            StreamWriter jsonWriter = new StreamWriter(_mediaPath);
-            // jsonWriter.Write();
+            StreamWriter missingJsonWriter = new StreamWriter(_mediaPath);
+            missingJsonWriter.Write(JsonConvert.SerializeObject(new MediaJsonStruct()));
+            missingJsonWriter.Close();
         }
 
-        string mediaJsonString = File.ReadAllText(_mediaPath);
-        MediaJsonStructure mediaStructure = JsonConvert.DeserializeObject<MediaJsonStructure>(mediaJsonString);
+        // convert json to a MediaJsonStruct
+        MediaJsonStruct mediaStructure = GetMedia();
 
+        // add the media to the correct list and increment the current id of that list
+        if(media is Film film){
+            film.Id = ++mediaStructure.CurrentFilmId;
+            mediaStructure.Films.Add(film);
+        }else if(media is Serie serie){
+            serie.Id = ++mediaStructure.CurrentSerieId;
+            mediaStructure.Series.Add(serie);
+        }
 
-        // StreamWriter jsonWriter = new StreamWriter(_mediaPath);
+        // save the MediaJsonStruct to the json file
+        SetMedia(mediaStructure);
 
-
-
-        return false;
+        return true;
     }
 
-    // current Id    LastInsertedId = 2032
-    // [2033,
-    // .[filmsList]]
+    private static MediaJsonStruct GetMedia(){
+        string mediaJsonString = File.ReadAllText(_mediaPath);
+        return JsonConvert.DeserializeObject<MediaJsonStruct>(mediaJsonString);
+    }
 
-    // [[serieId, []], [filmId, []]]
-}
-
-public struct MediaJsonStructure
-{
-    public KeyValuePair<int, List<Film>> Films { get; set; } = new KeyValuePair<int, List<Film>>(-1, new List<Film>());
-    public KeyValuePair<int, List<Serie>> Series { get; set; } = new KeyValuePair<int, List<Serie>>(-1, new List<Serie>());
-
-    public MediaJsonStructure(){}
-    public MediaJsonStructure(KeyValuePair<int, List<Film>> films, KeyValuePair<int, List<Serie>> series)
-    {
-        this.Films = films;
-        this.Series = series;
+    private static void SetMedia(MediaJsonStruct mediaStructure){
+        StreamWriter jsonWriter = new StreamWriter(_mediaPath);
+        jsonWriter.Write(JsonConvert.SerializeObject(mediaStructure));
+        jsonWriter.Close();
     }
 }
