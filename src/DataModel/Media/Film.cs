aa5b276c6cae72f4f1dff8d1e6e9db0f4212add8
Rosux <jcsbt2015@gmail.com>
2024-06-06 22:03:51 +0200

    Media model changed

--- src/DataModel/Media/Film.cs
+++ src/DataModel/Media/Film.cs
@@ -5,9 +5,18 @@ using Newtonsoft.Json;
 /// </summary>
 public class Film : Media
 {
-    public List<string> Actors = new List<string>();
-
-    public List<string> Writers = new List<string>();
+    public int Id { get; set; } = -1;
+    public string Title { get; set; } = "";
+    public int Runtime { get; set; } = -1;
+    public string Description { get; set; } = "";
+    public float Rating { get; set; } = 0f; // ranges from 0.0f to 10.0f
+    public string Language { get; set; } = "";
+    public List<Genre> Genres { get; set; } = new List<Genre>();
+    public DateOnly ReleaseDate { get; set; } = DateOnly.MinValue;
+    public Certification Certification { get; set; } = Certification.NONE;
+    public List<string> Directors { get; set; } = new List<string>();
+    public List<string> Actors { get; set; } = new List<string>();
+    public List<string> Writers { get; set; } = new List<string>();
 
     /// <summary>
     /// Creates a new film with the specified data.
@@ -26,8 +35,18 @@ public class Film : Media
     /// <param name="writers">A list of writers.</param>
     /// <returns>The created film.</returns>
     [JsonConstructor]
-    public Film(int id, string title, int runtime, string description, float rating, string language, List<Genre> genres, DateOnly releaseDate, Certification certification, List<string> directors, List<string> actors, List<string> writers) : base(id, title, runtime, description, rating, language, genres, releaseDate, certification, directors)
+    public Film(int id, string title, int runtime, string description, float rating, string language, List<Genre> genres, DateOnly releaseDate, Certification certification, List<string> directors, List<string> actors, List<string> writers)
     {
+        this.Id = id;
+        this.Title = title;
+        this.Runtime = runtime;
+        this.Description = description;
+        this.Rating = rating;
+        this.Language = language;
+        this.Genres = genres;
+        this.ReleaseDate = releaseDate;
+        this.Certification = certification;
+        this.Directors = directors;
         this.Actors = actors;
         this.Writers = writers;
     }
@@ -47,8 +66,18 @@ public class Film : Media
     /// <param name="actors">A list of actors.</param>
     /// <param name="writers">A list of writers.</param>
     /// <returns>The created film.</returns>
-    public Film(string title, int runtime, string description, float rating, string language, List<Genre> genres, DateOnly releaseDate, Certification certification, List<string> directors, List<string> actors, List<string> writers) : base(-1, title, runtime, description, rating, language, genres, releaseDate, certification, directors)
+    public Film(string title, int runtime, string description, float rating, string language, List<Genre> genres, DateOnly releaseDate, Certification certification, List<string> directors, List<string> actors, List<string> writers)
     {
+        this.Id = -1;
+        this.Title = title;
+        this.Runtime = runtime;
+        this.Description = description;
+        this.Rating = rating;
+        this.Language = language;
+        this.Genres = genres;
+        this.ReleaseDate = releaseDate;
+        this.Certification = certification;
+        this.Directors = directors;
         this.Actors = actors;
         this.Writers = writers;
     }
