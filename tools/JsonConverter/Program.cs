f70aa73ef60f28f4b67b691ae70bbb99c2ceb43f
Rosux <jcsbt2015@gmail.com>
2024-06-10 11:50:42 +0200

    json tool converter set up

---
+++ tools/JsonConverter/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace JsonConverter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Converting Films...");
+        string filmJsonString = File.ReadAllText("../tmdb_movies.json");
+        Console.WriteLine(filmJsonString);
+    }
+}
